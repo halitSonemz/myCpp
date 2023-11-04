c038fb7427f641c0892b650f7031345721836083
khalid_ <149331188+halit2023@users.noreply.github.com>
2023-11-05 00:58:24 +0100

    Add files via upload

---
+++ Cppthis.cpp
@@ -0,0 +1,28 @@
+class CppCalc
+{
+    private :
+        int result ;
+    public :
+        CppCalc ():result(0){}
+        CppCalc& firstCalc(int& first){
+            result += first ;
+            return *this ;
+        }
+        
+        CppCalc& secondCalc(int& second){
+            result += second ;
+            return *this ;
+        }
+        
+        int CppgetT(CppCalc& ObjectCalc){
+            return ObjectCalc.result ;
+        }
+    
+};
+
+int main()
+{
+    int first = 15 , second = 20 ;
+    CppCalc ObjectCalc  ;
+    int Cppresult = ObjectCalc.firstCalc(first).secondCalc(second).CppgetT(ObjectCalc);
+  
