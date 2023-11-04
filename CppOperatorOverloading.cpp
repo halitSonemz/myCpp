2a3f0f330c1417d790aec1147c0a08363f550ec3
khalid_ <149331188+halit2023@users.noreply.github.com>
2023-11-04 18:04:51 +0100

    Update CppOperatorOverloading.cpp

--- CppOperatorOverloading.cpp
+++ CppOperatorOverloading.cpp
@@ -4,6 +4,8 @@
 
 using namespace std ;
 
+
+//example 1 : 
 class Calculator
 {
     private  : 
@@ -30,4 +32,47 @@ int main()
     Calculator calculator_2(10) ;
     Calculator total = calculator_1 + calculator_2 ;
     cout<<"total is : "<<total ;
+}
+
+
+cout<<"=====================================================================" ;
+
+//example 2 :
+
+// Example program
+#include <iostream>
+#include <string>
+
+using namespace std ;
+
+class calculator{
+    private :
+        int first , second ;
+    public :
+        calculator(int first_  , int second_) : first(first_), second(second_){}
+        
+        calculator operator +(const calculator& Object){
+            calculator result(Object.first , Object.second) ;
+            return result ;
+        }
+        
+        void printfunc(calculator* Object){
+            cout<<Object->first+Object->second ;
+        }
+        
+};
+
+int main()
+{
+    int first , second ;
+    
+    cout<<"Enter your first number : "<<" ";
+    cin>>first ;
+    cout<<"Enter your second number : "<<" ";
+    cin>>second ;
+    
+    calculator*  calculator_1 = new calculator(first,second) ;
+    cout<<"total sum is : "; 
+    calculator_1->printfunc(calculator_1);
+    delete calculator_1 ;
 }
