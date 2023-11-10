662b8786d0aa21492420136153c871ee5be2846b
khalid_ <149331188+halit2023@users.noreply.github.com>
2023-11-10 01:08:28 +0100

    Update Cppthis.cpp

--- Cppthis.cpp
+++ Cppthis.cpp
@@ -26,4 +26,48 @@ int main()
     CppCalc ObjectCalc  ;
     int Cppresult = ObjectCalc.firstCalc(first).secondCalc(second).CppgetT(ObjectCalc);
 }
+
+
+
+
+cout<"*****************************************************************************************"<<endl;
+
+//example 2 : 
+
+// Example program
+#include <iostream>
+#include <string>
+
+using namespace std ;
+
+
+class Calculator
+{
+    private :
+        int first_number ;
+        int second_number ;
+        
+    public :
+        Calculator(int first_ , int second_) : first_number(first_), second_number(second_) {} 
+        int SimpleCalcul ()const 
+        {
+            auto lambda = [this](){
+                return first_number - second_number ;
+            };
+            return lambda() ;
+        }
+        
+        int getresult() const {
+            return SimpleCalcul() ;
+        }
+};
+
+int main()
+{
+    int first = 2023 ;
+    int second = 1997 ;
+    
+    Calculator  Calculator_(first,second);
+    cout<<"Sum result is  :"<<Calculator_.getresult() ;
+}
   
