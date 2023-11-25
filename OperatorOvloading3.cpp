41e24f117c49172b14ad1b8f9f9af16b48a734c6
Halit Sonemz <149331188+halitSonemz@users.noreply.github.com>
2023-11-25 14:51:11 +0100

    Add files via upload

---
+++ OperatorOvloading3.cpp
@@ -0,0 +1,73 @@
+// Example program : calculate your age using operator overloading 
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+class Calculator{
+    
+    private :
+        int CurrentYear ;
+        int BirthYear ;
+    public :
+        Calculator(int first_,int second_ ){
+            this->CurrentYear = first_ ;
+            this->BirthYear = second_ ; 
+        }
+        
+        int operator()(Calculator& Object){
+            return Object.CurrentYear - Object.BirthYear  ;
+        }
+        
+        int displayingSuB(Calculator& Object){
+            return operator()(Object) ;
+        }
+};
+
+int main(){
+    int CurrentYear = 2023 ;
+    int BirthYear= 1995 ;
+    int resultSuB ;
+    
+    Calculator  Calculator_(CurrentYear,BirthYear) ;
+    resultSuB = Calculator_.displayingSuB(Calculator_) ;
+    cout<<"your age is :  "<<resultSuB ;
+}
+        
+        // Example program : calculate your age using operator overloading 
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+class Calculator{
+    
+    private :
+        int CurrentYear ;
+        int BirthYear ;
+    public :
+        Calculator(int first_,int second_ ){
+            this->CurrentYear = first_ ;
+            this->BirthYear = second_ ; 
+        }
+        
+        int operator()(Calculator& Object){
+            return Object.CurrentYear - Object.BirthYear  ;
+        }
+        
+        int displayingSuB(Calculator& Object){
+            return operator()(Object) ;
+        }
+};
+
+int main(){
+    int CurrentYear = 2023 ;
+    int BirthYear= 1995 ;
+    int resultSuB ;
+    
+    Calculator  Calculator_(CurrentYear,BirthYear) ;
+    resultSuB = Calculator_.displayingSuB(Calculator_) ;
+    cout<<"your age is :  "<<resultSuB ;
+}
+        
+        
